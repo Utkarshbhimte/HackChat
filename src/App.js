f3295f2a90fecaf5e04e393e8e5506dfd9648676
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2017-10-20 05:15:19 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Routes from "./routes";
+
+// Redux Stuff
+import { Provider } from "react-redux";
+import store from "./store.js";
+
+import "./styles/dever-chat-theme.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
     );
   }
 }
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Redirect, Route, Router } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+// import Homepage from './pages/Homepage/Homepage.jsx'
+// React Component
+import asyncComponent from "./components/AsyncComponent";
+
+const history = createBrowserHistory();
+
+const AsyncHomepage = asyncComponent(() =>
+  import("./pages/Homepage/Homepage.jsx")
+);
+
+/* Use components to define routes */
+export default () => (
+  <Router history={history}>
+    <div className="contain-all">
+      <div className="page-wrap">
+        <Route exact path="/" render={() => <AsyncHomepage />} />
+      </div>
+    </div>
+  </Router>
+);
