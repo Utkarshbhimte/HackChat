bb1cb9aaaf52c7a0298757eca00b0bfc7d133328
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2017-10-20 09:27:48 +0530

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import Routes from "./routes";
 
+// Firebase
+import "./firebase";
+
 // Redux Stuff
 import { Provider } from "react-redux";
 import store from "./store.js";
+
+// Components
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 import "./styles/dever-chat-theme.css";
 
@@ -11,7 +17,12 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Routes />
+        <div className="contain-all">
+          <Navbar />
+          <div className="page-wrap">
+            <Routes />
+          </div>
+        </div>
       </Provider>
     );
   }
